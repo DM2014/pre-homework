9a4f01773b12596c56a669599f0a32dbdce24674
Ting-Yen Lai <banacorn@gmail.com>
2014-03-13 19:08:28 +0800

    String -> Lazy ByteString, 6x speedup

--- src/main.hs
+++ src/main.hs
@@ -1,31 +1,35 @@
 module Main where
 
-import Data.Ord (comparing, Down(..))
+import Data.Ord (Down(..))
 import Data.List (sortBy)
 import qualified Data.HashMap.Lazy as H
 import Data.Hashable (Hashable)
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Lazy (ByteString)
 
-type Loc = Int
-type User = Int
+type Loc = ByteString
+type User = ByteString
 type CheckIn = (User, Loc)
 type Table a = H.HashMap a Int
 
+main :: IO ()
 main = do
-    content <- readFile "data/loc.txt"
+    content <- BL.readFile "data/loc.txt"
 
-    let userTable = accumulate fst (parseCheckIn content)
-    let locTable = accumulate snd (parseCheckIn content)
+    let userTable     = accumulate fst (parseCheckIn content)
+    let locationTable = accumulate snd (parseCheckIn content)
 
     putStrLn "top 50 users:\n"
     print (top50 userTable)
-    --putStrLn "top 50 locations:\n"
-    --print . take 50 . sortBy (comparing (Down . snd)) . H.toList $ locTable
-
+    putStrLn "top 50 locations:\n"
+    print (top50 locationTable)
 
 -- Parse and make a list of CheckIns
-parseCheckIn :: String -> [CheckIn]
-parseCheckIn = map (toLoc . words) . lines
-    where toLoc xs = (read (head xs), read (last xs))
+parseCheckIn :: ByteString -> [CheckIn]
+parseCheckIn = map (toLoc . splitWords) . filter (not . BL.null) . splitLines 
+    where   splitLines = BL.split 0xa
+            splitWords = BL.split 0x9
+            toLoc xs = (head xs, last xs)
 
 -- Builds a table which counts the occurence of items with first argument being the selector
 accumulate :: (Eq a, Hashable a) => (CheckIn -> a) -> [CheckIn] -> Table a
